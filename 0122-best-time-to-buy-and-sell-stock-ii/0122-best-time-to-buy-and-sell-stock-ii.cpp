e709205759292333617851688d821be802084647
Tusshar Singh <tusshar580@gmail.com>
2026-09-09 22:40:09 +0530

    Time: 0 ms (100%), Space: 20 MB (84.03%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+
+        int buy = prices[0];
+        int profit = 0;
+
+        for (int price : prices) {
+
+            buy = min(buy, price);
+
+            if (price > buy) {
+                profit += price - buy;
+                buy = price;
+            }
+        }
+
+        return profit;
+    }
+};
